bc5e02c3c0a0836aa63bdfeab15e25c26c75036f
Ricardo Perovano <ricardoperovano@gmail.com>
2021-01-13 22:31:07 -0300

    backup

--- app/Config/backup.php
+++ app/Config/backup.php
@@ -1,5 +1,7 @@
 <?php
 
+use Spatie\DbDumper\Compressors\GzipCompressor;
+
 return [
 
     'backup' => [
@@ -42,72 +44,20 @@ return [
                 'ignore_unreadable_directories' => false,
             ],
 
-            /*
-             * The names of the connections to the databases that should be backed up
-             * MySQL, PostgreSQL, SQLite and Mongo databases are supported.
-             *
-             * The content of the database dump may be customized for each connection
-             * by adding a 'dump' key to the connection settings in config/database.php.
-             * E.g.
-             * 'mysql' => [
-             *       ...
-             *      'dump' => [
-             *           'excludeTables' => [
-             *                'table_to_exclude_from_backup',
-             *                'another_table_to_exclude'
-             *            ]
-             *       ],
-             * ],
-             *
-             * If you are using only InnoDB tables on a MySQL server, you can
-             * also supply the useSingleTransaction option to avoid table locking.
-             *
-             * E.g.
-             * 'mysql' => [
-             *       ...
-             *      'dump' => [
-             *           'useSingleTransaction' => true,
-             *       ],
-             * ],
-             *
-             * For a complete list of available customization options, see https://github.com/spatie/db-dumper
-             */
             'databases' => [
                 'mysql',
             ],
         ],
 
-        /*
-         * The database dump can be compressed to decrease diskspace usage.
-         *
-         * Out of the box Laravel-backup supplies
-         * Spatie\DbDumper\Compressors\GzipCompressor::class.
-         *
-         * You can also create custom compressor. More info on that here:
-         * https://github.com/spatie/db-dumper#using-compression
-         *
-         * If you do not want any compressor at all, set it to null.
-         */
-        'database_dump_compressor' => null,
+        'database_dump_compressor' => GzipCompressor::class,
 
         'destination' => [
 
-            /*
-             * The filename prefix used for the backup zip file.
-             */
-            'filename_prefix' => '',
-
-            /*
-             * The disk names on which the backups will be stored.
-             */
+            'filename_prefix' => env('BACKUP_NAME', 'knowledge_base') . '-',
             'disks' => [
                 'backup',
             ],
         ],
-
-        /*
-         * The directory where the temporary files will be stored.
-         */
         'temporary_directory' => storage_path('app/backup-temp'),
     ],
 
